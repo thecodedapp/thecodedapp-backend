0aea060f9719fd4bafc740dfca3c9c3454de0c1e
thecodedapp <contact@learnmaco.com>
2026-09-16 18:55:18 -0400

    Mount progress API

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import authRouter from "./routes/auth";
+import progressRouter from "./routes/progress";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.use("/auth", authRouter);
+app.use("/progress", progressRouter);
 
 app.get("/health", (_req, res) => {
   res.json({ ok: true });
